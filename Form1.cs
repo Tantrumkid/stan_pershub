c4cd17243bd56c2bd5211c0ee5b481f0bf80ad4f
St4nl3y <oshh997@gmail.com>
2022-04-01 21:44:20 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StanHub {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            this.Text = "";
+            this.ShowIcon = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        Settings settings;
+        PersHub hub;
+
+
+        private void Form1_Load(object sender, EventArgs e) {
+            settings = new Settings();
+            if (settings.LoadDefault()) {
+                hub = new PersHub(settings);
+            } else {
+                hub = new PersHub(new Settings() { Username = "Newbie", Screen = 0, Title = "Welcome to StanHub" });
+            }
+            
+
+
+
+
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StanHub {
+    class Settings {
+
+        private string title = "";
+        private string username = "";
+        private string saveLocal = "";
+        private int screen = 0;
+
+        public string Title { get => title; set => title = value; }
+        public string Username { get => username; set => username = value; }
+        public string SaveLocal { get => saveLocal; set => saveLocal = value; }
+        public int Screen { get => screen; set => screen = value; }
+
+        public void Init() {
+
+        }
+
+        public bool Load() {
+            bool curr = false;
+
+            return curr;
+        }
+
+        public bool LoadDefault() {
+            bool curr = false;
+
+            return curr;
+        }
+
+        public bool usingPrimaryScreen() {
+            if(Screen == 0) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+
+    }
+}
